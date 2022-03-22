e7396a4e93f2dd50c9a326e04d7fdf363c8e06f6
williamgodin <w.godin53@gmail.com>
2022-03-22 12:03:29 +0100

    Button in nav-bar works

--- src/Controller/MonController.php
+++ src/Controller/MonController.php
@@ -21,8 +21,8 @@ class MonController extends AbstractController
 
 
         //Récupération des casting
-        $em = $doctrine ->getManager();
-        $castingsREPO = $em -> getRepository(Casting::class);
+        $em = $doctrine->getManager();
+        $castingsREPO = $em->getRepository(Casting::class);
         $castings = $castingsREPO->findAll();
 
 
@@ -32,8 +32,9 @@ class MonController extends AbstractController
 
 
     }
+
     #[Route('/casting/{id}', name: 'app_casting')]
-    public function casting(ManagerRegistry $doctrine,$id): Response
+    public function casting(ManagerRegistry $doctrine, $id): Response
     {
         //Récupération du casting
         $em = $doctrine->getManager();
@@ -43,8 +44,8 @@ class MonController extends AbstractController
 
 
         //Récupération des casting
-        $em = $doctrine ->getManager();
-        $castingsREPO = $em -> getRepository(Casting::class);
+        $em = $doctrine->getManager();
+        $castingsREPO = $em->getRepository(Casting::class);
         $castings = $castingsREPO->findAll();
         return $this->render('mon/casting.html.twig', [
             'casting' => $casting[0],
@@ -52,5 +53,74 @@ class MonController extends AbstractController
         ]);
 
 
+    }
+
+    #[Route('/cinema', name: 'app_cinema')]
+    public function cinema(ManagerRegistry $doctrine): Response
+    {
+        //Récupération des castings cinéma
+        $em = $doctrine->getManager();
+        $query = $em->createQuery('SELECT casting FROM App\Entity\Casting casting WHERE casting.Metier = 1');
+        $castingcinema = $query->getResult();
+
+        //Récupération des casting
+        $em = $doctrine->getManager();
+        $castingsREPO = $em->getRepository(Casting::class);
+        $castings = $castingsREPO->findAll();
+        return $this->render('mon/cinema.html.twig', [
+            'castingcinema' => $castingcinema,
+            'castings' => $castings,
+        ]);
+    }
+    #[Route('/musique', name: 'app_musique')]
+    public function musique(ManagerRegistry $doctrine): Response
+    {
+        //Récupération des castings musique
+        $em = $doctrine->getManager();
+        $query = $em->createQuery('SELECT casting FROM App\Entity\Casting casting WHERE casting.Metier = 2');
+        $castingmusique = $query->getResult();
+
+        //Récupération des casting
+        $em = $doctrine->getManager();
+        $castingsREPO = $em->getRepository(Casting::class);
+        $castings = $castingsREPO->findAll();
+        return $this->render('mon/musique.html.twig', [
+            'castingmusique' => $castingmusique,
+            'castings' => $castings,
+        ]);
+    }
+    #[Route('/danse', name: 'app_danse')]
+    public function danse(ManagerRegistry $doctrine): Response
+    {
+        //Récupération des castings danse
+        $em = $doctrine->getManager();
+        $query = $em->createQuery('SELECT casting FROM App\Entity\Casting casting WHERE casting.Metier = 3');
+        $castingdanse = $query->getResult();
+
+        //Récupération des casting
+        $em = $doctrine->getManager();
+        $castingsREPO = $em->getRepository(Casting::class);
+        $castings = $castingsREPO->findAll();
+        return $this->render('mon/danse.html.twig', [
+            'castingdanse' => $castingdanse,
+            'castings' => $castings,
+        ]);
+    }
+    #[Route('/theatre', name: 'app_theatre')]
+    public function theatre(ManagerRegistry $doctrine): Response
+    {
+        //Récupération des castings theatre
+        $em = $doctrine->getManager();
+        $query = $em->createQuery('SELECT casting FROM App\Entity\Casting casting WHERE casting.Metier = 4');
+        $castingtheatre = $query->getResult();
+
+        //Récupération des casting
+        $em = $doctrine->getManager();
+        $castingsREPO = $em->getRepository(Casting::class);
+        $castings = $castingsREPO->findAll();
+        return $this->render('mon/theatre.html.twig', [
+            'castingtheatre' => $castingtheatre,
+            'castings' => $castings,
+        ]);
     }
 }
